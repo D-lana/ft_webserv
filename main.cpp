4baf62928756d0d152989b9cf97a2126e2c9c271
Piri Veeta <pveeta@student.21-school.ru>
2022-09-21 18:57:46 +0300

    edit main

--- main.cpp
+++ main.cpp
@@ -16,6 +16,8 @@
 
 int main(int argc, const char* argv[])
 {
+	
+	// проверка валидности
 	if (argc != 2) {
         std::cerr << "No configuration file" << std::endl;
         return 0;
@@ -37,11 +39,12 @@ int main(int argc, const char* argv[])
     }
 	
 	
-	// /* код девочек
+	// /* небольшие правки в то, что было в ветке, с учетом парсера 
 
+	// int count_server = 2;
 	int count_server = parser->getServers().size();
 	// std::cout << "CHECK: count_server = " << count_server << std::endl;
-	// int ports[3] = {8080, 8090};
+	// int ports[3] = {8080, 8090};  // храню порты в векторе
 
 	// config
 
