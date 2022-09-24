837a550601d0f481931450f957e9dee012924b3c
obeedril <obeedril@student.21-school.ru>
2022-09-24 15:29:03 +0300

    24.09

--- Request.cpp
+++ Request.cpp
@@ -60,14 +60,16 @@ Request::Request(std::string& _buffer){
             valueHead = tmpStr.substr(delimiter + 2, tmpStr.length() - keyHead.length()-2);
             headers.insert(std::pair<std::string, std::string> (keyHead, valueHead));
             buffer.erase(0, pos+2);
-            // str = "";
-            std::cout << "buffer0 |" << buffer << "|" << std::endl;
+           
+            // std::cout << "buffer0 |" << buffer << "|" << std::endl;
         }
     }
 
     void Request::bodyParsing(){
 
         std::ifstream ifs;
+        std::size_t posStartBody = 0;
+        bool openFile = false;
 
         ifs.open ("/tmp/tmpfile", std::ifstream::in|std::ifstream::out);
 
@@ -89,12 +91,32 @@ Request::Request(std::string& _buffer){
             std::string str;
             while (getline(ifs, str)) {
             // getline(ifs, str);
-            if ((i = str.find("filename=")) != std::string::npos) {
-                // str.erase(0, pos+10);
-                i += 10;
-                filename = str.substr(i, str.find("\""));
-                // ifs.erase(0, ifs.find("\r\n\r\n") + 4);
-            }
+                if (str.find("filename=") != std::string::npos) {
+                    // str.erase(0, pos+10);
+                    i += 10;
+                    filename = str.substr(i, str.find("\""));
+                    // ifs.erase(0, ifs.find("\r\n\r\n") + 4);
+                }
+                if (str.find(endBoundary) != std::string::npos) {
+                    break;
+                }
+                if (str.find(boundary) != std::string::npos) {
+                    std::ofstream fout;
+                    openFile = true;
+                    // posStartBody = i + boundary.length();
+                    // if ((posStartBody = str.find("\r\n\r\n", i)) != std::string::npos){
+                    // posStartBody += 4;
+                    // }
+
+                    // fout.open("upload/" + filename);
+                    // fout << str.substr(i);
+
+                    // while ()
+                }
+                if (str.find("\r\n\r\n") && openFile == true){
+                    posStartBody = i + boundary.length();
+                }
+                i += str.length();
 
 
             // std::ofstream fout;
